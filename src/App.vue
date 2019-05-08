2aaf96d0686ec6e7853c44ebd9d64011c6958de5
JewelBreeveld <rjbreeveld@gmail.com>
2019-05-08 11:10:20 +0200

    Created todos component to display list of items

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Todos v-bind:todos="todos"/>
+  </div>
 </template>
 
 <script>
+import Todos from "./components/Todos";
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ]
+    };
   }
 };
 </script>
